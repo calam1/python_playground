b9fc2eb4aaf7154c0e216035b661d38f972904d7
Christopher Lam <chrislam67@gmail.com>
2016-01-17 15:38:56 -0600

    some lesson 2 stuff from udacity

--- udacity/cs212/lesson1/zebra.py
+++ udacity/cs212/lesson1/zebra.py
@@ -1,5 +1,31 @@
 #incomplete
+import itertools
+
+print "BASEC ASSIGNMENT"
 houses = first, _, middle, _, _ = [1, 2, 3, 4, 5]
 print houses
 print first
 print middle
+print "----------------"
+
+print "ITERTOOLS.PERMUTATIONS"
+orderings = list((itertools.permutations(houses)))
+print orderings
+print "----------------"
+
+print "YIELD"
+def f123():
+    yield 1
+    yield 2
+    yield 3
+
+for item in f123():
+    print item
+
+print "----------------"
+#print "?????"
+#def c(sequence):
+#    c.starts += 1
+#    for item in sequence:
+#        c.items += 1
+#        yield item
